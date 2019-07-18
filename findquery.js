243cac0dcc1955639c771c95295f5d39c92dc6b0
Manik Hosen <rahathossenmanik@gmail.com>
2019-07-18 23:30:07 +0600

    Query

---
+++ findquery.js
@@ -0,0 +1,13 @@
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
+MongoClient.connect(url, { useNewUrlParser: true }, function(err, db) {
+	if (err) throw err;
+	var dbo = db.db("NodeDB");
+	var query = { address: "Park Lane 38" };
+	var myobj = { name: "Company Inc", address: "Highway 37" };
+	dbo.collection("Customers").find(query, { projection: { _id: 0}}).toArray(function(err, result) {
+		if (err) throw err;
+		console.log(result);
+		db.close();
+	});
+});
